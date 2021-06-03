c741eb131b8d94257220fdcc809717f215bf90d1
Jon Narine <jonnicwolf@gmail.com>
2021-06-02 23:09:24 -0400

    finished problems added

--- isIsogram.js
+++
@@ -1,9 +0,0 @@
-const isIsogram = (str) => {
-  const arr = str.split("");
-  const compare = Array.from(new Set(arr));
-  if (compare.length !== arr.length) {
-    return false;
-  } else {
-    return true;
-  }
-};
--- thirdLargestNum.js
+++
@@ -1,8 +0,0 @@
-//Write a function that takes in an array of numbers and returns the 3rd largest number
-
-const thirdLargestNumber = (arrOfNums) => {
-    const sorted = arrOfNums.sort().reverse()
-    return sorted[2]
-}
-
-console.log(thirdLargestNumber([7,2,5,1,9,8,3]))
