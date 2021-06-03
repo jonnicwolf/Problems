d0faf39b157ca810aa325838c6332ff3288a02d9
Jon Narine <jonnicwolf@gmail.com>
2021-06-03 05:22:51 -0400

    looped 2x, removed duplice sets from twoSum

--- compareTriplets.js
+++ compareTriplets.js
@@ -1,19 +1,29 @@
+// const compareTriplets = (a, b) => {
+//   const score = {
+//     alice: 0,
+//     bob: 0,
+//   };
+//   for (let i = 0; i < a.length; i++) {
+//     console.log(score);
+//     if (a[i] > b[i]) {
+//       score.alice++;
+//       console.log(score.alice);
+//     } else if (a[i] < b[i]) {
+//       score.bob++;
+//       console.log(score.bob);
+//     }
+//   }
+//   return Object.values(score);
+// };
+
 const compareTriplets = (a, b) => {
-  const score = {
-    alice: 0,
-    bob: 0,
-  };
-  for (let i = 0; i < a.length; i++) {
-    console.log(score);
-    if (a[i] > b[i]) {
-      score.alice++;
-      console.log(score.alice);
-    } else if (a[i] < b[i]) {
-      score.bob++;
-      console.log(score.bob);
-    }
-  }
-  return Object.values(score);
-};
+    let [i, alice, bob] = [0,0,0]
+    while (i < alice.length) {
+        switch {
+            
+        }
+    }    
+    return [alice,bob]
+}
 
 console.log(compareTriplets([1, 2, 3], [3, 2, 1]));
--- twoSum.js
+++ twoSum.js
@@ -5,27 +5,24 @@
 // You can return the answer in any order.
 
 const twoSum = (nums, target) => {
-  const neuArr = [];
-  const resultNums = nums.map((x) => {
-    for (let i = 0; i < nums.length - 1; i++) {
-      if (x + nums[i] === target) {
-        neuArr.push([x, nums[i]]);
-        console.log(neuArr);
+  const arr = []
+  for (let i = 0; i < nums.length; i++){
+    for (let j = 0; j < nums.length; j++){
+      if ((nums[i] + nums[j]) === target) {
+        console.log(nums[i],nums[j])
+        arr.push(nums[i], nums[j])
       }
-      // (x + nums[i] === target) ? neuArr.push(x, nums[i]) : null;
     }
-  });
-  index = 0;
-//   newObj = {}
-  while (index < neuArr.length) {
-    // newObj = neuArr[index]
-    // console.log(newObj)
-    
-    index++
-    
-      
   }
-  return neuArr
+  console.log(arr)
+  const result = Array.from(new Set(arr))
+  while (result.length > 0) {
+
+    let set = result.splice(0, 2)
+    console.log(set)
+  }
+  console.log(set)
+  return set
 };
 //input: [1,2,3] 5
 //output: [2,3]
