09544292e0d210ce92d86a0b42622153aff4120e
Jon Narine <jonnicwolf@gmail.com>
2021-06-06 22:15:09 -0400

    more explicit conditions for n

--- nearHundred.js
+++ nearHundred.js
@@ -1,27 +1,39 @@
-//Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+// //Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
 
-const nearHundred = (num) => {
-    let bool 
-    if (num <= 210) {
-        if (num >= 190) {
-            bool = true
-        }
-        else if (num >= 90) {
-            bool = true
-        }
-        else {
-            bool = false
-        }
+// const nearHundred = (num) => {
+//     let bool 
+//     if (num <= 210) {
+//         if (num >= 190) {
+//             bool = true
+//         }
+//         else if (num >= 90) {
+//             bool = true
+//         }
+//         else {
+//             bool = false
+//         }
+//     }
+//     else if (num <= 110) {
+//         if (num >= 90) {
+//             bool = true
+//         }
+//     }
+//     else {
+//         bool = false
+//     }
+//     return bool
+// }
+
+// console.log(nearHundred(111))
+
+const nearHundred = (n) => {
+    if (n <= 210 && n >= 190) {
+        return true
     }
-    else if (num <= 110) {
-        if (num >= 90) {
-            bool = true
-        }
+    else if (n <= 110 && n >= 90) {
+        return true
     }
     else {
-        bool = false
+        return false
     }
-    return bool
 }
-
-console.log(nearHundred(111))
