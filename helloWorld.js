e900858958b20dae511559d12f579ee74be20fce
Jon Narine <jonnicwolf@gmail.com>
2021-06-09 15:22:46 -0400

    capitalise H and W

--- helloWorld.js
+++ helloWorld.js
@@ -6,7 +6,16 @@ const helloWorld = () => {
     //separate off from the ',' after hello -> hello,
     //add a space after the comma 
     //return our string
-    const helloworldArr = Array.from( String.fromCharCode(72,69,76,76,79,188,32,87,79,82,76,68))
+    const helloworldArr = Array.from(String.fromCharCode(72, 69, 76, 76, 79,  32, 87, 79, 82, 76, 68).toLowerCase())
+    for (let i = 0; i < helloworldArr.length; i++) {
+        if (helloworldArr[i] === 'h') {
+            helloworldArr[i]=helloworldArr[i].toUpperCase()
+        }
+        else if (helloworldArr[i] === 'w') {
+            helloworldArr[i]=helloworldArr[i].toUpperCase()
+        }
+    }
+    
     console.log(helloworldArr)
 }
 console.log(helloWorld())
