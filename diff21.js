b1cae5574787a24b27de51ad7c17b65cc2f0d9b5
Jon Narine <jonnicwolf@gmail.com>
2021-06-10 04:19:04 -0400

    Math.abs to find absolute difference

--- diff21.js
+++ diff21.js
@@ -1,11 +1,19 @@
 // Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+// const diff21 = (n) => {
+//     if (n > 21) {
+//         return (21-n)*2
+//     }
+//     else {
+//         return (21-n)
+//     }    
+// }
 const diff21 = (n) => {
     if (n > 21) {
-        return (21-n)*2
+        return Math.abs(n-21)*2
     }
     else {
-        return (21-n)
-    }    
+        return Math.abs(n-21)
+    }
 }
 console.log(diff21(19))
 console.log(diff21(10))
