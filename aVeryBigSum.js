798c01e974ee13b9cffc4ff6e8623f4f0d997792
Jon Narine <jonnicwolf@gmail.com>
2021-06-12 19:48:48 -0400

    move all files into problems dir

--- aVeryBigSum.js
+++
@@ -1,9 +0,0 @@
-//return the sum of a given array of numbers
-const aVeryBigSum = (ar) => {
-    const sum =  ar.reduce((acc, el)=> {
-        return acc + el
-    })
-    return sum
-}
-
-console.log(aVeryBigSum([1, 2, 3, 4,9,8,3, 4,9]))
