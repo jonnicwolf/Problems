b24efbbd3f1f708ed6032e5e6d8e703aa10ed270
Jon Narine <jonnicwolf@gmail.com>
2021-07-03 21:31:37 -0400

    skips white space but capitalises letter after whitespace x2

--- wave.js
+++ wave.js
@@ -3,13 +3,22 @@ const wave = str => {
     let output = []
     let sample = [...str]
     for (let i = 0; i < sample.length; i++){
-        const cap = sample[i].toUpperCase()
-        sample.splice(i,1,cap)
-        output.push(sample.join(''))
-        const low = sample[i].toLowerCase()
-        sample.splice(i,1,low)
+        if (sample[i] === ' ') {
+            const cap = sample[i + 1].toUpperCase()
+            sample.splice(i + 1, 1, cap)
+            output.push(sample.join(''))
+            const low = sample[i].toLowerCase()
+            sample.splice(i, 1, low)
+        }
+        else {
+            const cap = sample[i].toUpperCase()
+            sample.splice(i, 1, cap)
+            output.push(sample.join(''))
+            const low = sample[i].toLowerCase()
+            sample.splice(i, 1, low)
+        }
     }
     return output
 }
 
-console.log(wave('hello'))
+console.log(wave('hello world'))
