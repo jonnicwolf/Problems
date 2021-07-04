0569838a5e5add908960337abb515e4d5d2d1312
Jon Narine <jonnicwolf@gmail.com>
2021-07-03 21:43:22 -0400

    when idx = whitespace do nothing

--- wave.js
+++ wave.js
@@ -4,11 +4,7 @@ const wave = str => {
     let sample = [...str]
     for (let i = 0; i < sample.length; i++){
         if (sample[i] === ' ') {
-            const cap = sample[i + 1].toUpperCase()
-            sample.splice(i + 1, 1, cap)
-            output.push(sample.join(''))
-            const low = sample[i].toLowerCase()
-            sample.splice(i, 1, low)
+            null
         }
         else {
             const cap = sample[i].toUpperCase()
@@ -21,4 +17,4 @@ const wave = str => {
     return output
 }
 
-console.log(wave('hello world'))
+console.log(wave('The quick brown fox jumped over the lazy dog'))
