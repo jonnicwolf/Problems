6582b92a6c94d55fda76bd715d728688d3a31bd0
Jon Narine <jnwnarine@gmail.com>
2022-01-27 15:30:14 -0500

    colorGhost done

---
+++ ghostClass.js
@@ -0,0 +1,66 @@
+class Ghost {
+    color;
+  }
+  
+  const colors = ['white','yellow','purple','red']
+  const RNG = Math.floor(Math.random() * colors.length-1)+1
+  
+  ghost = new Ghost()
+  ghost.color = colors[RNG]
+
+  console.log(ghost)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  1
+
+
+
+
+  
+
+
+
+
+
+  
