55b623bb3dd20e8b7b852a0c0342f36724ac2ae5
Jon Narine <jnwnarine@gmail.com>
2022-01-28 00:36:10 -0500

    validSpacing passing all tests

--- validSpacing.js
+++ validSpacing.js
@@ -8,37 +8,28 @@ and no leading or trailing spaces. Below are some examples of what the function 
  */
 
 function validSpacing(s){
-    // //when the string is trimmed, is it the same as the og string?
-    // const ends = s.trim() === s ? true : false;   console.log(ends)
-    // //does s have extra strings in the middle?
-    // console.log(s.split(' ').includes(' '))
-    // if (ends) {return  s.split(' ').includes(' ') ? false : true}
-    // else {return s.split(' ').includes(' ') ? false : true}
-
-    //when the string is trimmed, is it the same as s?
-    let result
-    const is_extraSpace = s.split(' ').includes(' ')
-    console.log(is_extraSpace)
-    const ends = s.trim() === s 
-        ? s.split(' ').includes(' ') 
-            ? result = false
-            : result = true
-        : result = false;
-    return result
-    //therefore s.trim() is s
-    //does s contain extra spaces after split?
+    const endSpaces_exist = s.trim() === s ? false : true; console.log(endSpaces_exist)
+    const extraSpacesInside_exist = s.split(' ').includes(''); console.log(extraSpacesInside_exist)
     
+    //do spaces exist on either end?
+    if (endSpaces_exist === false){
+        if (extraSpacesInside_exist === false){
+            return true;
+        } 
+        else if (s === '') return true
+        else return false;
+    } else return false;
 };
 
-// console.log(validSpacing('Hello world'))//    = true
-// console.log(validSpacing(' Hello world '))//   = false
-// console.log(validSpacing('Hello world  '))//  = false
+console.log(validSpacing('Hello world'))//    = true
+console.log(validSpacing(' Hello world '))//   = false
+console.log(validSpacing('Hello world  '))//  = false
 console.log(validSpacing('Hello  world'))//   = false
-// console.log(validSpacing('Hello'))//          = true
-// console.log(validSpacing('Helloworld'))//     = true 
-// console.log(validSpacing('Helloworld '))//    = false
-// console.log(validSpacing(' '))/// = false
-// console.log(validSpacing(''))//// = true
-// console.log(validSpacing('ORG  X'))
-// console.log(validSpacing('S   O h  f'))
-// console.log(validSpacing('ONt  nWi'))//false
+console.log(validSpacing('Hello'))//          = true
+console.log(validSpacing('Helloworld'))//     = true 
+console.log(validSpacing('Helloworld '))//    = false
+console.log(validSpacing(' '))/// = false
+console.log(validSpacing(''))//// = true
+console.log(validSpacing('ORG  X'))
+console.log(validSpacing('S   O h  f'))
+console.log(validSpacing('ONt  nWi'))//false
