02e1342f937b954204e256cc9929fe29190ff88c
Jon Narine <jnwnarine@gmail.com>
2022-02-04 17:40:13 -0500

    sortNumbers done

--- well2.js
+++ well2.js
@@ -1,15 +1,15 @@
 /**
- For every good kata idea there seem to be quite a few bad ones!
+For every good kata idea there seem to be quite a few bad ones!
 
 In this kata you need to check the provided 2 dimensional array (x) f
-or good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, 
-return 'Publish!', if there are more than 2 return 'I smell a series!'.
- If there are no good ideas, as is often the case, return 'Fail!'.
+    or good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, 
+    return 'Publish!', if there are more than 2 return 'I smell a series!'.
 
+If there are no good ideas, as is often the case, return 'Fail!'.
 The sub arrays may not be the same length.
 
-The solution should be case insensitive (ie good, 
-    GOOD and gOOd all count as a good idea). All inputs may not be strings.
+
+The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings).
  */
 
 function well2(x){
@@ -17,11 +17,11 @@ function well2(x){
     let counter = {};
     for (let val of superX){
         if (counter[val]){
-            counter[val]++
+            counter[val]++;
         } else counter[val] = 1
     };    
     return counter.good <= 2 ? 'Publish!' : counter.good > 2 ? 'I smell a series!' : 'Fail!'
-}
+};
 
 
 console.log(well2([['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad']]));//fail
