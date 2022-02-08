103f30f28c7600a1aa3d7f788b7c6d4441709788
Jon Narine <jnwnarine@gmail.com>
2022-02-08 03:47:34 -0500

    gooseFilter done

--- gooseFilter.js
+++ gooseFilter.js
@@ -12,97 +12,10 @@
 // The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 //  */
 
-// function gooseFilter(birds){
-//     const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-//     let result = birds
-//     for (i = 0; i < geese.length; i++){
-//         for (j = 0; j < result.length; j++){
-//             if (geese[i]===result[j]){
-//                 result.splice(j,1)
-//             }
-//         }
-//     }
-//     return result;
-// }
+function gooseFilter(birds){
+    const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter((bird)=>!geese.includes(bird));    
+};
 
-// console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]))
-// console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]))
-
-
-const myResult = [ 'Crested',
-  'Barbary',
-  'Toulouse',
-  'Mallard',
-  'Hook Bill',
-  'Crested',
-  'Pilgrim',
-  'Crested',
-  'Blue Swedish',
-  'Crested',
-  'Crested',
-  'Hook Bill',
-  'Mallard',
-  'Hook Bill',
-  'Mallard',
-  'Mallard',
-  'Mallard',
-  'Mallard',
-  'Hook Bill',
-  'Mallard',
-  'Roman Tufted',
-  'Toulouse',
-  'Barbary',
-  'Mallard',
-  'Mallard',
-  'Crested',
-  'Barbary',
-  'Hook Bill',
-  'Blue Swedish' ]
-
-
-  const expected1 = [ 'Crested',
-  'Barbary',
-  'Mallard',
-  'Hook Bill',
-  'Crested',
-  'Crested',
-  'Blue Swedish',
-  'Crested',
-  'Crested',
-  'Hook Bill',
-  'Mallard',
-  'Hook Bill',
-  'Mallard',
-  'Mallard',
-  'Mallard',
-  'Mallard',
-  'Hook Bill',
-  'Mallard',
-  'Barbary',
-  'Mallard',
-  'Mallard',
-  'Crested',
-  'Barbary',
-  'Hook Bill',
-  'Blue Swedish' ]
-
-  console.table(expected1)
-
-
-function countBirds (flockOne, flockTwo){
-    const rookOne = {};
-    const rookTwo = {};
-    for (let bird of flockOne){
-        if (rookOne[bird]){
-            rookOne[bird]++
-        } else { rookOne[bird]=1 };
-    }
-    for (let bird of flockTwo){
-        if (rookTwo[bird]){
-            rookTwo[bird]++
-        } else { rookTwo[bird]=1 };
-    }    
-    console.log(rookOne, rookTwo)
-}
-
-countBirds(myResult,expected1)
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]))
+console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]))
