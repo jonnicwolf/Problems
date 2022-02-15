6219a1ac82d13c14fa86f6efc3ac776b47af8a5d
Jon Narine <jnwnarine@gmail.com>
2022-02-14 21:38:36 -0500

    total done

--- total.js
+++ total.js
@@ -7,14 +7,26 @@ function total(arr){
     if (arr.length === 0){
         return 0;
     } else {
-        function isPrime()
-    };
+        function isPrime(num) {
+            for(var i = 2; i < num; i++) 
+                if(num % i === 0) return false;              
+                return num > 1;
+          }        
+        let counter = 0;
+
+        for(i=2;i<arr.length;i++){            
+            if(isPrime(i)){                
+                counter += arr[i]
+            };
+        };
+        return counter;
+    };    
 };
 
-// console.log(total([]))//,0);
+console.log(total([]))//,0);
 console.log(total([1,2,3,4]))//,7);
-// console.log(total([1,2,3,4,5,6]))//,13);
-// console.log(total([1,2,3,4,5,6,7,8]))//,21);
-// console.log(total([1,2,3,4,5,6,7,8,9,10,11]))//,21);
-// console.log(total([1,2,3,4,5,6,7,8,9,10,11,12,13]))//,33);
-// console.log(total([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))//,47);
+console.log(total([1,2,3,4,5,6]))//,13);
+console.log(total([1,2,3,4,5,6,7,8]))//,21);
+console.log(total([1,2,3,4,5,6,7,8,9,10,11]))//,21);
+console.log(total([1,2,3,4,5,6,7,8,9,10,11,12,13]))//,33);
+console.log(total([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))//,47);
