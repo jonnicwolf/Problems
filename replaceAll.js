c1edebdca74e7bbbf48b51965abbacba56e883fc
Jon Narine <jnwnarine@gmail.com>
2022-03-31 04:53:04 -0400

    replaceAll progress

--- replaceAll.js
+++ replaceAll.js
@@ -8,51 +8,52 @@ The function will always be called with three strings,
     so don't worry about validating the arguments.
 
 As with the original "string".replace(find, replace), if find is an empty string, 
-    it should match in-between each character, effectively inserting replace in-between e
-    ach character, including the start and end of the string.
+    it should match in-between each character, effectively inserting replace in-between 
+    each character, including the start and end of the string.
      I've given you some tests for this to make it a little clearer.
  */
 
 function replaceAll(input, find, replace){
-    // let result = [];
-    // if (find === '') {
-    //     result = input.split('')                
-    //     for (let i=0;i < input.length+1;i++){
-    //         console.log(result)
-    //         if (result[i] !== find){
-    //             result.splice(i,0,replace)
-    //             i++
-    //         }
-    //     }
-    // } else result = input.split(' ');    
-    // console.log(result)
-    // console.log(result)
     
-    let inputToArr = input.split(' ');    
-    console.log(inputToArr)
+    if (input === ''){ //if input is empty, go in between
+        let inputToArr = input.split(' '); 
+        inputToArr = inputToArr.map(word=> word.replace(find,replace));      
+        return inputToArr
+    }
+    if (input.split(' ').length === 1){
+        
+        //check that the one element contains find
+        // inputToArr = String(inputToArr)
+        // inputToArr = String(inputToArr).split('').join(' ').split('')
+        // inputToArr.push(replace);
+        // inputToArr.unshift(replace);
+        // inputToArr = inputToArr.map((word)=> { if (find === '') return word.replace(' ', replace)} );
+        
+        let result = input
+        console.log(result)
+        result = result.replace(find,replace)
+        console.log(result)
+        
+        
+        // do {
+        //     result = result.replace(find, replace)
+        // } while (result.includes(find));
+        // console.log(result)
+        // return result;
 
-    if (inputToArr.length === 1) {
-        inputToArr = String(inputToArr).split('').join('').split('');
-        inputToArr.push('');
-        inputToArr.unshift('');
-
-        inputToArr = inputToArr.map((word)=> {
-            if (find = ''){
-                console.log(word.replace('',replace))
-            }
-            console.log(inputToArr)
-            ;
-        })
-        console.log(inputToArr)
-        return inputToArr.join(' ')}
-
-    else { 
-        inputToArr = inputToArr.map(word=> word.replace(find,replace))
-        return inputToArr.join('')}
+    } 
     
-}
+    else {              
+        let inputToArr = input.split(' '); //works
+        inputToArr = inputToArr.map(word => word.replace(find,replace));
+        return inputToArr.join(' ');
+    }        
+};
 
 // console.log(replaceAll("", "", "-"))//, "-", "Empty input, empty find");
-// // console.log(replaceAll("1", "", "-"))//, "-1-", "Single-character input, empty find");
-console.log(replaceAll("123", "", "-"))//, "-1-2-3-", "Empty string as find");//undefined
-// console.log(replaceAll('Darien Matthew Morales Darien', 'Darien', "Monkey")) //fix spacing
+// console.log(replaceAll("1", "", "-"))//, "-1-", "Single-character input, empty find");
+ 
+// console.log(replaceAll('Darien Matthew Morales Darien', 'Darien', "Monkey"))
+// console.log(replaceAll("string-string", "ing", "!"))
+
+console.log(replaceAll("123", "", "-"))
