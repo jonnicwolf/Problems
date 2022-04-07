dadd0c8da1d41fb790914f4ecf913e845e934212
Jon Narine <jnwnarine@gmail.com>
2022-04-07 18:56:12 -0400

    split each num and convert ea elem to charcode

--- strangeMath.js
+++ strangeMath.js
@@ -24,13 +24,45 @@ strangeMath(15, 5) === 11
 strangeMath(15, 15) === 7
  */
 
-function strangeMath (n,k){
-   let result = []
-   for (i = 1;i <= n;i++) {
-      result.push(String(i).charCodeAt(0))   
+// function strangeMath (n,k){
+//    let result = [];
+//    for (i = 1;i <= n;i++) {      
+//       result.push(String(i));  
+//    }
+//    result = 
+//    result      
+//       .map(num=>{
+//          return num.length < result[result.length-1].length
+//             ? `${num}0`
+//             : `${num}0`;
+//       })
+//       .sort((a,b)=>a[0]-b[0])
+//       .map(num=>{
+//          num = num.split('')   
+//          return num.join('')
+//       })
+//    for(let i = 0;i<result.length;i++){
+//       result[i] = result[i].split('')
+//       result[i].length--
+//       result[i] = result[i].join('')
+//    }
+//    // console.log(result)
+//    return result.indexOf(String(k))+1;
+// }
+
+
+function strangeMath(n,k){
+   let result = [];
+   for (let i = 1;i<=n;i++){
+      result.push(String(i).split('').map((num,i)=>num.charCodeAt(0)))
    }
-   result = result.sort((a,b)=> a-b)
-   console.log(result)
+   console.log(result.sort().map(num=>String.fromCharCode(num)))
+
+   
+      
 }
 
-console.log(strangeMath(11, 2))
+console.log(strangeMath(11, 2));
+// console.log(strangeMath(15, 5));
+// console.log(strangeMath(15, 15));
+// console.log(strangeMath(39298, 20735));
