4ebd5c1bcbf82d9b50d35da42e37e0d78408be56
Jon Narine <jnwnarine@gmail.com>
2022-05-16 22:26:25 -0400

    housekeeping

--- isValidIP.js
+++ isValidIP.js
@@ -21,27 +21,56 @@ function isValidIP (str){
     const arrayOfOctetsAndDots = str.split('');
     let octetStack = [];
     let arrayOfOctets = [];
-    for (let i = 0; i < arrayOfOctetsAndDots.length; i++){        
-        if (arrayOfOctetsAndDots[i]!=='.'){
-            octetStack.push(arrayOfOctetsAndDots[i]);    
-            if (Number(octetStack.join(''))>255 || Number(octetStack.join(''))<0) return false;
-            if (octetStack.length===2 && octetStack[0]==='0') return false;
-            if (arrayOfOctets.length>=3 && octetStack.length===3) arrayOfOctets.push(octetStack.join(''));
+    for (let i = 0; i < arrayOfOctetsAndDots.length; i++){                
+        if (arrayOfOctetsAndDots[i]!=='.') {            
+            octetStack.push(arrayOfOctetsAndDots[i]);  
         }
         else if (arrayOfOctetsAndDots[i]==='.') {
-            arrayOfOctets.push(octetStack.join(''));
+            let octetPush = octetStack.join('');            
+            console.log(octetPush)
+            if (
+                octetPush<0 
+                || octetPush>255                 
+                || octetPush.includes(' ')) {
+                    return false;
+            }
+            if (octetPush.length>1 && octetPush[0]==='0') {
+                return false;
+            };                     
+            arrayOfOctets.push(octetPush);              
             octetStack.length = 0;
         };        
-    };    
-    if (arrayOfOctets.length===4){
+        if (i === arrayOfOctetsAndDots.length-1) {            
+            let octetPush = octetStack.join('');            
+            if (
+                octetPush<0 || 
+                octetPush>255 ||
+                
+                octetPush.includes(' ')) {
+                    return false;
+            }
+            if (octetPush<0 && octetPush>=255) {
+                return false;
+            };
+            arrayOfOctets.push(octetPush)
+        };
+    };            
+    if (arrayOfOctets.length>4){
         return false;
     };    
     return true;
 };
-// console.log(isValidIP('0.0.0.0'))
-// console.log(isValidIP('1.2.3.4'))
-// console.log(isValidIP('123.45.67.89'))
-// console.log(isValidIP('1.2.3.4.5'))
-// console.log(isValidIP('123.456.78.90'))
-// console.log(isValidIP('123.045.067.089'))
+console.log(isValidIP('137.255.156.100')) // true
+console.log(isValidIP('0.0.0.0')) //true
+console.log(isValidIP('123.45.67.89')) //true
+console.log(isValidIP('1.2.3.4.5')) //false
+console.log(isValidIP('123.456.78.90')) //false
+console.log(isValidIP('123.045.067.089')) //false
+console.log(isValidIP(' 1.2.3.4'))//false
+console.log(isValidIP('1e1.033.lsk.alsk'))
 
+function numberCheck (str) {
+    return Number.isNaN(str)
+}
+
+console.log(numberCheck("NaN"))
