fe607e3f8766f1b80e12ed58e3633864ca2d61a7
Jon Narine <jnwnarine@gmail.com>
2022-05-18 03:56:06 -0400

    solved

--- isValidIP.js
+++ isValidIP.js
@@ -17,91 +17,49 @@ Leading zeros (e.g. 01.02.03.04) are considered invalid
 Inputs are guaranteed to be a single string
  */
 
-// function isValidIP (str){
-//     const arrayOfOctetsAndDots = str.split('');
-//     let octetStack = [];
-//     let arrayOfOctets = [];
-//     for (let i = 0; i < arrayOfOctetsAndDots.length; i++){                
-//         if (arrayOfOctetsAndDots[i]!=='.') {            
-//             octetStack.push(arrayOfOctetsAndDots[i]);  
+// function isValidIP(str) {    
+//     if (str[0]==='.' || str[str.length-1]==='.') return false; 
+//     let [dotCount, segment, arrayOfSegments] = [0, [], []];    
+//     for (let i=0; i<str.length; i++){
+//         let currentChar = str.charCodeAt(i);
+//         if(str[i]==='.' && str[i+1]==='.') return false;         
+//         if (currentChar===46) {              
+//             dotCount++;            
+//             let completeSegment = segment.join('');               
+//             if (Number(completeSegment)<0 || Number(completeSegment)>255 || completeSegment===[]) return false;            
+//             if (completeSegment.length>1 && completeSegment[0]==0) return false;            
+//             arrayOfSegments.push(completeSegment);           
+//             segment.length = 0;
 //         }
-//         else if (arrayOfOctetsAndDots[i]==='.') {
-//             let octetPush = octetStack.join('');                        
-//             if (
-//                 octetPush<0 ||
-//                 octetPush>255 ||                
-//                 octetPush.includes()) {
-//                     return false;
-//             }
-//             if (octetPush.length>1 && octetPush[0]==='0') {
-//                 return false;
-//             };                     
-//             arrayOfOctets.push(octetPush);              
-//             octetStack.length = 0;
-//         };        
-//         if (i === arrayOfOctetsAndDots.length-1) {            
-//             let octetPush = octetStack.join('');            
-//             if (
-//                 octetPush<0 || 
-//                 octetPush>255 ||                
-//                 octetPush.includes(' ')) {
-//                     return false;
-//             }
-//             if (octetPush<0 && octetPush>=255) {
-//                 return false;
-//             };
-//             arrayOfOctets.push(octetPush)
+//         else if (currentChar>=48 && currentChar<=57 && currentChar!==46) segment.push(str[i])            
+//         else return false;        
+//         if (i===str.length-1) {            
+//             let completeSegmentAsNumber = Number(segment.join(''));
+//             if (completeSegmentAsNumber<0 || completeSegmentAsNumber>255 || (segment.length>1 && segment[0]==='0')) return false;
+//             arrayOfSegments.push(completeSegmentAsNumber);
 //         };
-//     };            
-//     if (arrayOfOctets.length>4){
-//         return false;
 //     };    
-//     return true;
+//     return (dotCount===3 && arrayOfSegments.length===4) ? true : false;
 // };
-
-function isValidIP(str) {
-    let dotCount = 0;
-    let segment = [];
-    let arrayOfSegments = [];
-    for (let i=0; i<str.length; i++){
-        let currentChar = str.charCodeAt(i)
-        
-        if (currentChar===46) {            
-            dotCount++;            
-            let completeSegment = segment.join('');                        
-            console.log(Number(completeSegment))
-            if (Number(completeSegment)<0 || Number(completeSegment)>255 || completeSegment===[]) {                
-                return false
-            };            
-            if (completeSegment.length>1 && completeSegment[0]==0) return false;            
-            arrayOfSegments.push(completeSegment);            
-            segment.length = 0;
-        }
-        
-        else if (currentChar>=48 && currentChar<=57 && currentChar!==46) {
-            console.log(currentChar)
-            segment.push(str[i])
-        }    
-        else return false;
-        
-        if (i===str.length-1) {            
-            let completeSegmentAsNumber = Number(segment.join(''))
-            console.log(segment)
-            console.log(completeSegmentAsNumber)
-            if (completeSegmentAsNumber<0 || completeSegmentAsNumber>255) return false;
-            arrayOfSegments.push(completeSegmentAsNumber)
-        };
-    };
-    console.log(dotCount, arrayOfSegments.length)
-    return dotCount===3 && arrayOfSegments.length===4 ? true : false;
-}
-console.log(isValidIP('137.255.156.100')) //true
-console.log(isValidIP('1.2.3.4')) //true
-console.log(isValidIP('0.0.0.0')) //true
-console.log(isValidIP('123.45.67.89')) //true
-console.log(isValidIP('1.2.3.4.5')) //false
-console.log(isValidIP('123.456.78.90')) //false
-console.log(isValidIP('123.045.067.089')) //false
-console.log(isValidIP(' 1.2.3.4')) //false
-console.log(isValidIP('1e1.033.lsk.alsk')) //false
-console.log(isValidIP('abc.def.ghi.jkl')) //false
+function isValidIP (str){
+    const octets = str.split('.');
+    return octets.length===4 && octets.every(octet=> Number(octet)<256 && String(Number(octet))===octet);
+};
+// console.log(isValidIP('49.189..211')) //false
+// console.log(isValidIP('245..7.47')) //false
+// console.log(isValidIP('105..129.134')) //false
+// console.log(isValidIP('.217.29.97')) //false
+// console.log(isValidIP('.141.70.33')) //false
+// console.log(isValidIP('50.88.33.')) //false
+// console.log(isValidIP('62.106.74.')) //false
+// console.log(isValidIP('137.255.156.100')) //true
+// console.log(isValidIP('1.2.3.4')) //true
+// console.log(isValidIP('0.0.0.0')) //true
+// console.log(isValidIP('123.45.67.89')) //true
+// console.log(isValidIP('1.2.3.4.5')) //false
+// console.log(isValidIP('123.456.78.90')) //false
+// console.log(isValidIP('123.045.067.089')) //false
+// console.log(isValidIP(' 1.2.3.4')) //false
+// console.log(isValidIP('1e1.033.lsk.alsk')) //false
+// console.log(isValidIP('abc.def.ghi.jkl')) //false
+// console.log(isValidIP('84.33.50.052')) //false
