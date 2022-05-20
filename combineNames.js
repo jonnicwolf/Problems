d3a4081e35041b50c94c68e20bd203fc62b755a4
Jon Narine <jnwnarine@gmail.com>
2022-05-20 06:08:02 -0400

    solved

---
+++ combineNames.js
@@ -0,0 +1,14 @@
+/**
+ * Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+ */
+
+function combineNames (name1,name2) {
+    return `${name1} ${name2}`
+}
