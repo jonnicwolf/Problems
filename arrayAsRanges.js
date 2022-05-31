011b4839fa66db1df15e221f4648ffc73ee1bfab
Jon Narine <jnwnarine@gmail.com>
2022-05-31 07:12:36 -0400

    some progress

--- arrayAsRanges.js
+++ arrayAsRanges.js
@@ -9,53 +9,7 @@
 
 For example,
 The numbers 5, 6, 7, 8 and 9 would be displayed as "5_9"
-The number 6 would just be "6"
- */
-
-function toRange(arr){    
-    let neuArr = arr.sort((a,b)=> a-b) ;    
-    let result;
-    for (let i = 0; i < neuArr.length-1; i++){                
-        if (neuArr[i+1]-neuArr[i]===1) result = `${neuArr[0]}_${neuArr[neuArr.length-1]}`
-        else result = `${neuArr[0]}_${neuArr[i]},${neuArr[neuArr.length-1]}`
-    }
-    return result;
-};
-
-
-// function toArray(str){
-//     // convert str to an array ✅
-//     // figure out if its a pure range or not ✅
-//     // separate the extra num if impure ✅
-//     // then fill in the nums between the underscore ✅
-//     // then add extra number at the end of new array ✅
-
-//     let range = str.split('') ;
-//     if (range.includes(',')){
-//         let extraNum = [] ;
-//         extraNum.push(range[range.length-1]);
-//         range.length-=2;
-//         for (let i = 0; i < range.length; i++){
-//             if (range[i]==="_") range.splice(i,1)                    
-//         };
-//         range = range.map(val=>Number(val));
-//         let result = [];
-//         for (let i = range[0]; i <= range[range.length-1]; i++){            
-//             result.push(i);
-//         };
-//         return [...result,Number(extraNum)];
-//     } else {
-//         let result = [];
-//         for (let i = 0; i < range.length; i++){
-//             if (range[i]==='_') range.splice(i,1);
-//         };
-//         range = range.map(val=> Number(val));
-//         for (let i = range[0]; i <= range[range.length-1]; i++){
-//             result.push(i);
-//         };
-//         return result;
-//     };
-// };
+The number 6 would just be "6"*/
 
 function toRange (arr){
     let numberStack = []
@@ -79,12 +33,24 @@ function toRange (arr){
     return range.join('')
 }
 
-// function toArray(str){
-
-// }
-console.log(toRange([1,2,3,4,5,6,9]))
-// console.log(toArray(toRange([1,2,3,4,5,6,9])))
-console.log(toRange([1,2,3,4,5]))
+function toArray(str){    
+    console.log(str.split(','))
+    let strArray = str.split('')
+    let range = []
+    let result = []
+    for (let i=0;i<strArray.length;i++){
+        if (strArray[i]==='_'){
+            range.push(strArray[i-1],strArray[i+1])
+        }        
+    }
+    for (let i=range[0]; i<=range[1];i++){
+        result.push(i)
+    }
+    
+}
+// console.log(toRange([1,2,3,4,5,6,9]))
+console.log(toArray(toRange([1,2,3,4,5,6,9])))
+// console.log(toRange([1,2,3,4,5]))
 // console.log(toArray(toRange([1,2,3,4,5])))
 
 
