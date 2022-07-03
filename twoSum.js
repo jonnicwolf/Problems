34dd7828ae29e630e2930d5c9e5fa1323110421d
Jon Narine <jnwnarine@gmail.com>
2022-07-03 04:59:17 -0400

    solved

--- twoSum.js
+++ twoSum.js
@@ -4,57 +4,16 @@
 
 // You can return the answer in any order.
 
-// const twoSum = (nums, target) => {
-//   const arr = [];
-//   for (let i = 0; i < nums.length; i++) {
-//     for (let j = 0; j < nums.length; j++) {
-//       if (nums[i] + nums[j] === target) {
-//         arr.push(nums[i], nums[j]);
-//       }
-//     }
-//   }
-//   const result = Array.from(new Set(arr))
-
-//   let filter;
-//   let [i,j] = [0,0]
-//   while (i < result.length) {
-//     filter = result.splice(i, j + 1);
-//     i++
-//     j++
-//   }
-//   return filter;
-// };
-//input: [1,2,3] 5
-//output: [2,3]
-
-// console.log(twoSum([1, 2, 3,4,5], 5));
-
-// const twoSum = function(nums, target) {
-//   const comp = {};
-//   for(let i=0; i<nums.length; i++){
-//       if(comp[nums[i] ]>=0){
-//           return [ comp[nums[i] ] , i]
-//       }
-//     comp[target - nums[i]] = i
-//   }
-// };
-// twoSum([3, 6], 9)
-
-//given an array of int return two indices such that they add up to target
-const twoSum = (nums, target) => {
-  let output = []
-  for (let i = 0; i < nums.length; i++){
-    for (let j = 0; j < nums.length; j++){
-      console.log(nums[j], nums[i])
-      
-      nums[j] + nums[i] === target
-        ? output.push([nums[i], nums[j]])
-        : null
+function twoSum (nums, target){
+  const potentials = {}
+  for (let num of nums){
+    if (potentials[target-num]){
+      return [target-num,num]
     }
-    console.log(output)
+    potentials[num] = num
   }
-  return output
-};
+}
+console.log(twoSum([2,7,11,15],9));
 
-const thru10 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(twoSum(thru10, 12));
+
+
